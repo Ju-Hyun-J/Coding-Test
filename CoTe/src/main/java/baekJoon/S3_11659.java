9f17e811aac5442d1786c3605bf11da68b736dc0
Ju-Hyun-J <jujung967@gmail.com>
2026-03-19 00:54:59 +0900

    [add] 11659 - 구간 합 구하기 4

---
+++ CoTe/src/main/java/baekJoon/S3_11659.java
@@ -0,0 +1,36 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S3_11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+        int[] sum = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum[i] = sum[i - 1] + arr[i];
+        }
+
+        for (int s = 0; s < M; s++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            sb.append(sum[j] - sum[i - 1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
